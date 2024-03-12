5f878f9d17da61884de4d5e8e02dc23aa496bf66
sejin0211 <sejin0211@naver.com>
2024-03-12 17:25:03 +0900

    COMMA project init setting

---
+++ src/main/java/com/coders/comma/CommaApplication.java
@@ -0,0 +1,13 @@
+package com.coders.comma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommaApplication.class, args);
+	}
+
+}
