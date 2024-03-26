a2bf9bf0d2279804ee72aab0bd390e7faf1f6de1
SunWoo <sunwoo3856@naver.com>
2024-03-26 15:13:10 +0900

    development environment settings

--- src/main/java/com/coders/comma/account/AccountMapper.java
+++
@@ -1,9 +0,0 @@
-package com.coders.comma.account;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-@Mapper
-public interface AccountMapper {
-    public Account getAccount(@Param("userid") String userid);
-}
