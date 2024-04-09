7bce06bbbd922c138b17f3e41f1b87e40277300d
sejin <sejin3510@gmail.com>
2024-04-09 15:45:33 +0900

    [ADDING] 상담센터 -> 심리검사 API 완성

---
+++ src/main/java/com/coders/comma/psy/psytest/PsyTestRepository.java
@@ -0,0 +1,6 @@
+package com.coders.comma.psy.psytest;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PsyTestRepository extends JpaRepository<PsyTest, Integer> {
+}
